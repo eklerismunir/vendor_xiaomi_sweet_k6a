c23e7641f580d5d9739f62085e547bb2ad99e51d
basamaryan <basam.aryan@gmail.com>
2023-10-29 22:03:57 +0100

    sweet: Import vendor/lib/vndk/libxlog.so
    Change-Id: I8013303b4e49fe6827feef4f1eec4cabc8796bca

--- sweet-vendor.mk
+++ sweet-vendor.mk
@@ -342,6 +342,7 @@ PRODUCT_PACKAGES += \
     libsmwrapper \
     libsndmonitor \
     libspkrprot \
+    libxlog \
     libvppclient \
     capi_v2_aptX_CLHDAD_Encoder \
     capi_v2_aptX_Classic \
